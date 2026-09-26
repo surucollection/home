834a39337d88a0bb5094c34bd258d6bba9c9fae4
surucollection <suruchisahani7@gmail.com>
2026-09-26 18:09:31 +0545

    Clean up static asset and page routes

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function CollectionCard({image,name,description}){
-  return <a className="collection-card" href="products.html"><div className="collection-image"><img src={"assets/"+image} alt={name} loading="lazy"/></div><div className="collection-content"><h3>{name}</h3><p>{description}</p></div></a>
+  return <a className="collection-card" href="/products.html"><div className="collection-image"><img src={"/assets/"+image} alt={name} loading="lazy"/></div><div className="collection-content"><h3>{name}</h3><p>{description}</p></div></a>
 }
